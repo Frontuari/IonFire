2d01697f62c664928e6425cdffebdffae1565ee1
Jorge Colmenarez <jlct.master@gmail.com>
2017-11-19 19:05:17 -0400

    Fixed file

--- src/pages/guia/guia.ts
+++ src/pages/guia/guia.ts
@@ -1,115 +1,49 @@
-page-home {
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
-}
+@Component({
+  selector: 'page-guia',
+  templateUrl: 'guia.html',
+})
+export class GuiaPage {
 
-.video-container {
-    position: relative;
-    padding-bottom: 56.25%;
-    padding-top: 30px; height: 0; overflow: hidden;
-}
-  
-.video-container iframe,
-.video-container object,
-.video-container embed {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-}
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
 
+  slides = [
+    {
+      title: "ANDDANDO TE DA LA BIENVENIDA",
+      description: "Descubre cómo organizar tu vida de manera equilibrada",
+      image: "assets/images/ANDDANDOMK600.png",
+      width:"240",
+      height:"188",
+    },
+    {      
+      title: "¿QUÉ HACES TODOS LOS DÍAS ?",
+      description: "Registra tus actividades diarias relacionadas con estas 7 categorías: Sueño, Alimento, Yo -Cuerpo y Mente-, Otros, Trabajo, Humanidad y Pareja. Asegúrate de incluir todo lo que haces a diario. incluyendo aquellas actividades que para ti podrían ocupar más de una categoría, lo importante es que seas consciente de lo que estás haciendo y seas tú quien lo decida.",
+      image: "assets/images/24-7.png",
+      width:"100",
+      height:"100",
+    },
+    {
+      title: "24 HORAS PARA SER HUMANO",
+      description: "Ingresa la cantidad de tiempo que inviertes durante el día a cada una estas actividades. Hazlo cada vez que finalices cualquier actividad, antes de dormir o de acuerdo a tus posibilidades. ¡Sólo te llevará 5 minutos!",
+      image: "assets/images/24HorasSerHumanos.jpg",
+      width:"150",
+      height:"150",
+    },
+    {
+      title: "¿CÓMO ESTOY ?",
+      description: "¡Conoce de manera gráfica la forma en la que vives a partir de lo que haces durante las 24 horas del día! <br />Observa cuáles son tus prioridades, a qué actividades dedicas más o menos horas al día y los posibles desequilibrios que puedan surgir a causa de cualquier distribución desproporcionada.",
+      image: "assets/images/Grafica2.png",
+      width:"240",
+      height:"135",
+    }
+  ];
 
-.perfil{
-    background-color: #212121;
-    width: auto;
-    height: 200px;
-    padding-top: 10px;  
-    color: #fff; 
-}
+  irHome() {
+    this.navCtrl.setRoot(HomePage);
+  }
 
-.circulo {
-  height: 50px;
-  width: 50px;
-  display: table-cell;
-  text-align: center;
-  font-size:18px;
-  vertical-align: middle;
-  border-radius: 50%;
-  background: #ffb744;
-}
-
-.conteo{
-    padding: 10px;
-}
-
-
-/* Círculos de colores numerados */
-span.red {
-  background: red;
-   border-radius: 0.8em;
-  -moz-border-radius: 0.8em;
-  -webkit-border-radius: 0.8em;
-  color: #ffffff;
-  display: inline-block;
-  font-weight: bold;
-  line-height: 1.6em;
-  margin-right: 15px;
-  text-align: center;
-  width: 1.6em; 
-}
-
-span.grey {
-  background: #cccccc;
-  border-radius: 0.8em;
-  -moz-border-radius: 0.8em;
-  -webkit-border-radius: 0.8em;
-  color: #fff;
-  display: inline-block;
-  font-weight: bold;
-  line-height: 1.6em;
-  margin-right: 15px;
-  text-align: center;
-  width: 1.6em; 
-}
-
-span.green {
-  background: #5EA226;
-  border-radius: 0.8em;
-  -moz-border-radius: 0.8em;
-  -webkit-border-radius: 0.8em;
-  color: #ffffff;
-  display: inline-block;
-  font-weight: bold;
-  line-height: 1.6em;
-  margin-right: 15px;
-  text-align: center;
-  width: 1.6em; 
-}
-
-span.blue {
-  background: #5178D0;
-  border-radius: 0.8em;
-  -moz-border-radius: 0.8em;
-  -webkit-border-radius: 0.8em;
-  color: #ffffff;
-  display: inline-block;
-  font-weight: bold;
-  line-height: 1.6em;
-  margin-right: 15px;
-  text-align: center;
-  width: 1.6em; 
-}
-
-span.pink {
-  background: #EF0BD8;
-  border-radius: 0.8em;
-  -moz-border-radius: 0.8em;
-  -webkit-border-radius: 0.8em;
-  color: #ffffff;
-  display: inline-block;
-  font-weight: bold;
-  line-height: 1.6em;
-  margin-right: 15px;
-  text-align: center;
-  width: 1.6em; 
 }
