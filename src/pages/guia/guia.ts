2c064e1fabb21f07ed65b9916b5426a642ea48a2
danielkheavy <danielkheavy@gmail.com>
2017-11-20 09:53:22 -0400

    Cambios en la  guia

--- src/pages/guia/guia.ts
+++ src/pages/guia/guia.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
 
 @Component({
@@ -15,30 +15,44 @@ export class GuiaPage {
     {
       title: "ANDDANDO TE DA LA BIENVENIDA",
       description: "Descubre cómo organizar tu vida de manera equilibrada",
-      image: "assets/images/ANDDANDOMK600.png",
-      width:"240",
-      height:"188",
+      image: "assets/images/Equilibrada.png",
+      width:"200",
+      height:"200",
     },
     {      
       title: "¿QUÉ HACES TODOS LOS DÍAS ?",
-      description: "Registra tus actividades diarias relacionadas con estas 7 categorías: Sueño, Alimento, Yo -Cuerpo y Mente-, Otros, Trabajo, Humanidad y Pareja. Asegúrate de incluir todo lo que haces a diario. incluyendo aquellas actividades que para ti podrían ocupar más de una categoría, lo importante es que seas consciente de lo que estás haciendo y seas tú quien lo decida.",
+      description: "Registra tus actividades diarias y la cantidad de tiempo que inviertes a cada una de ellas. Asegúrate de ingresar todo lo que hayas hecho, incluso aquello que para ti podría ocupar más de una categoría. Lo importante es que seas consciente de lo que haces y seas tú quien lo decida.",      
       image: "assets/images/24-7.png",
       width:"100",
       height:"100",
     },
     {
-      title: "24 HORAS PARA SER HUMANO",
-      description: "Ingresa la cantidad de tiempo que inviertes durante el día a cada una estas actividades. Hazlo cada vez que finalices cualquier actividad, antes de dormir o de acuerdo a tus posibilidades. ¡Sólo te llevará 5 minutos!",
+      title: "¿QUÉ HACES TODOS LOS DÍAS ?",
+      description: "Cada vez que finalices alguna actividad tómate un tiempo para anotar tus datos, también puedes hacerlo antes de dormir o de acuerdo a tus posibilidades. <br>¡Sólo te llevará 5 minutos!",
       image: "assets/images/24HorasSerHumanos.jpg",
-      width:"150",
-      height:"150",
+      width:"125",
+      height:"125",
+    },
+    {
+      title: "¿QUÉ QUIERO HACER?",
+      description: "Dedícate un momento para pensar en lo que te gusta y crea una guía de lo que quisieras hacer diariamente. Incluye actividades que desearías iniciar o retomar para que sean parte de tu rutina diaria. <br>¡Recuerda! Sin importar lo que hagas, haz lo que más te guste pero hazlo. ",
+      image: "assets/images/head.png",
+      width:"118",
+      height:"147",
     },
     {
       title: "¿CÓMO ESTOY ?",
-      description: "¡Conoce de manera gráfica la forma en la que vives a partir de lo que haces durante las 24 horas del día! <br />Observa cuáles son tus prioridades, a qué actividades dedicas más o menos horas al día y los posibles desequilibrios que puedan surgir a causa de cualquier distribución desproporcionada.",
-      image: "assets/images/Grafica2.png",
-      width:"240",
-      height:"135",
+      description: "Conoce de manera gráfica la forma en la que vives a partir de lo que haces durante las 24 horas del día. Cuáles son tus prioridades, qué desequilibrios puedes tener por la forma en la que ocupas el tiempo y cómo emplearlo de manera asertiva. <br> Una vez empieces a experimentar los beneficios del cambio que buscabas, disfruta y comparte tu proceso. ",
+      image: "assets/images/head2.png",
+      width: "126",
+      height: "141",
+    },
+    {
+      title: "EL ACTO ES LA VERDAD DE LA PALABRA",
+      description: "¡ANDDANDO!",
+      image: "assets/images/Celphone.png",
+      width: "108",
+      height: "109",
     }
   ];
 
