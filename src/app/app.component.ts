8a78ef2f91979ed51cad2d06de80449aa6df9e57
danielkheavy <danielkheavy@gmail.com>
2017-12-05 17:15:36 -0300

    Cambio de Posicion de Tab cambio de icono como estoy actualizacion de guia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -47,7 +47,7 @@ export class MyApp {
 
     this.pages = [
       { title: 'Inicio',        component: HomePage, icon: 'home', color: 'dark' },
-      { title: '¿Qué Quiero?', component: WhatDoIWant2Page, icon: 'md-switch', color: 'dark' },  
+      { title: '¿Qué Quiero?', component: WhatDoIWant2Page, icon: 'ios-book', color: 'dark' },  
       { title: '24/7', component: AgregarPage, icon: 'add-circle', color: 'dark' },      
       { title: '¿Cómo Estoy?',    component: TabsPage, icon: 'filing', color: 'dark' },/* paper, podium */
       { title: 'Guía rápida',     component: GuiaPage, icon: 'help', color: 'dark' },
